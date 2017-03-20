f81f2e66e0131fc034e0f57348ee8b435a636d88
tele1494 <tele1494@gmail.com>
2017-03-20 23:04:15 +0000

    Update contact-form-handler.php

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,24 +1,8 @@
 <?php
 
-$errors = '';
+
 $myemail = 'suleymaniye.1494@gmail.com';//<-----Put Your email address here.
-if(empty($_POST['email']) || 
-    empty($_POST['message']))
-    {
-        $errors .= "\n Error: all fields are required";
-    }
 
-$email_address = $_POST['email']; 
-$message = $_POST['message']; 
-if (!preg_match(
-    "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
-    $email_address))
-    {
-        $errors .= "\n Error: Invalid email address";
-}
-
-if( empty($errors))
-{
     $to = $myemail;
     $email_subject = "Contact form submission: Nombre";
     $email_body = "You have received a new message. ".
@@ -29,6 +13,6 @@ if( empty($errors))
     mail($to,$email_subject,$email_body,$headers);
     //redirect to the 'thank you' page
     header('Location: contact-form-thank-you.html');
-}
+
 
 ?>
