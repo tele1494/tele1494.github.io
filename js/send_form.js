3c4076f5a6b55362cf8bf89ee7566ccf56a611bc
tele1494 <tele1494@gmail.com>
2017-03-22 20:07:58 +0000

    Update send_form.js

--- js/send_form.js
+++ js/send_form.js
@@ -1,12 +1,10 @@
 $("#contactform").submit(function(e) {
     
-    var email = document.getElementById("inputemail"),
-        message = document.getElementById("inputmessage");
-    
     $.ajax({
         url: "https://formspree.io/suleymaniye.1494@gmail.com", 
         method: "POST",
         data: $(this).serialize(),
         dataType: "json"
     });
+    
 });
